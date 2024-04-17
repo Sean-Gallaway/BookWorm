488b172ae8e6f5a9a5e63607e428d75ab3e546f1
Potat-OS1 <apotat.os1@gmail.com>
2024-04-16 21:59:55 -0700

    forgot some documentation

--- src/main/java/com/bkgroup/worm/Card.java
+++ src/main/java/com/bkgroup/worm/Card.java
@@ -10,8 +10,12 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Card {
+    // size of the card.
     static double size = 300;
+    // potential future use.
     public static ArrayList<Card> cards = new ArrayList<>();
+
+
     @FXML
     private FlowPane title;
     @FXML
@@ -23,14 +27,9 @@ public class Card {
     @FXML
     private AnchorPane base;
 
-    @FXML
-    public void initialize () {
-
-    }
-
     /**
-     *
-     * @param text
+     * Separates the given text by spaces and places text objects into the description field. in the future we can add text effects if we wish.
+     * @param text the given text
      */
     private void populateDescription (String text) {
         String[] passage = text.split(" ");
@@ -41,14 +40,13 @@ public class Card {
     }
 
     /**
-     *
-     * @param p
-     * @param str
+     * Creates a card and places it into a container.
+     * @param p the given container
+     * @param str description
      */
     public static void getCard (Pane p, String str) {
         try {
             FXMLLoader loader = new FXMLLoader(Switcher.class.getResource("/card.fxml"));
-            loader.getNamespace().put("labelText", "reee");
             loader.getNamespace().put("size", size);
             p.getChildren().add(loader.load());
             Card cardController = loader.getController();
