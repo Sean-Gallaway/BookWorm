3576f2b56e064fbafdaeb5c1ad926f0154b145ba
Garrett Bassen <77715672+GarrettBassen@users.noreply.github.com>
2024-05-18 15:51:14 -0700

    Made basic Account Creation page
    Created basic account creation page with inputs for username, email, and password.
    
    Skeleton controller class added as well

---
+++ src/main/java/com/bkgroup/worm/controllers/AccountCreationController.java
@@ -0,0 +1,33 @@
+package com.bkgroup.worm.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AccountCreationController {
+
+    @FXML
+    private Button btnCart;
+    @FXML
+    private Button btnHome;
+    @FXML
+    private Button btnProfile;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private Button btn_createAccount;
+    @FXML
+    private TextField txt_email;
+    @FXML
+    private PasswordField txt_password;
+    @FXML
+    private TextField txt_password_confirm;
+    @FXML
+    private PasswordField txt_username;
+
+    @FXML
+    void CreateAccount() {
+
+    }
+}
