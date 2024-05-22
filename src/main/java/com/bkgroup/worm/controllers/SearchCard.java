e5502e8271ef89258b97ff8aae4935b627aaf0f8
Potat-OS1 <apotat.os1@gmail.com>
2024-05-21 20:10:25 -0700

    made the buttons for adding and removing from cart via the search

--- src/main/java/com/bkgroup/worm/controllers/SearchCard.java
+++ src/main/java/com/bkgroup/worm/controllers/SearchCard.java
@@ -1,18 +1,41 @@
 package com.bkgroup.worm.controllers;
 
-import javafx.fxml.FXML;
+import com.bkgroup.worm.utils.Tools;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 public class SearchCard {
+    public static Image cartImage = new Image(Tools.class.getResourceAsStream("/images/addToCart.png"));
+    public static Image removeImage = new Image(Tools.class.getResourceAsStream("/images/removeFromCart.png"));
     public Rectangle button;
     public ImageView image;
     public Label label;
+    boolean putInCart = false;
 
     void setup (Image img, String string) {
         image.setImage(img);
         label.setText(string);
+        button.setFill(new ImagePattern(cartImage));
+        button.setOnMouseClicked(event-> {
+            if (putInCart) {
+                System.out.println("remove from cart");
+                /*
+                Cole put you're method of removing from cart here
+                */
+                button.setFill(new ImagePattern(cartImage));
+                putInCart = false;
+            }
+            else {
+                System.out.println("add to cart");
+                /*
+                Cole put you're method of adding to cart here.
+                */
+                button.setFill(new ImagePattern(removeImage));
+                putInCart = true;
+            }
+        });
     }
 }
