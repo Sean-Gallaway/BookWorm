82792dabcd8284d2b7f638354cb67f4ab0e4b8b0
Potat-OS1 <apotat.os1@gmail.com>
2024-05-25 20:49:23 -0700

    change to the search so it uses a builder instead so i don't wanna neckrope myself looking at it.

--- src/main/java/com/bkgroup/worm/controllers/SearchController.java
+++ src/main/java/com/bkgroup/worm/controllers/SearchController.java
@@ -2,6 +2,7 @@ package com.bkgroup.worm.controllers;
 
 import com.bkgroup.worm.utils.DatabaseConnection;
 import com.bkgroup.worm.utils.Query;
+import com.bkgroup.worm.utils.SearchBuilder;
 import com.bkgroup.worm.utils.Tools;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -66,111 +67,20 @@ public class SearchController {
     public void searchQuery () {
         // genre search
         fp.getChildren().clear();
-        StringBuilder queryOne = new StringBuilder("""
-                \tSELECT a.bookID,
-                \t    b.title,
-                \t    COUNT(*) AS num
-                \tFROM genre a
-                \t    JOIN book b
-                \t        ON a.bookID = b.bookID""");
-        for (int a = 0; a < selectedGenres.size(); a++) {
-            if (a == 0) {
-                queryOne.append("\tWHERE\s");
-            }
-            queryOne.append(" a.genre = '");
-            queryOne.append(selectedGenres.get(a));
-            queryOne.append("'");
-            if (a != selectedGenres.size()-1) {
-                queryOne.append(" OR");
-            }
-        }
-        queryOne.append("\n\tGROUP BY a.bookID");
-        //
 
-        // exact name search
-        StringBuilder queryTwo = new StringBuilder("""
-                \tSELECT bookID,
-                \t    title,
-                \t    CEIL(COUNT(*)*3.5) as num
-                \tFROM book
-                \tWHERE\s""");
         String[] searchTerms = searchField.getText().split(" ");
-        for (int a = 0; a < searchTerms.length; a++) {
-            queryTwo.append(" title LIKE '%");
-            queryTwo.append(searchTerms[a]);
-            queryTwo.append("%'");
-            if (a != searchTerms.length-1) {
-                queryTwo.append(" AND");
-            }
+        SearchBuilder search = new SearchBuilder()
+                .fullName(searchTerms)
+                .publication((int)yearMin.getValue(), (int)yearMax.getValue())
+                .genre(selectedGenres.toArray(new String[0]));
+        for (String str : searchTerms) {
+            search.partialName(str);
         }
-        queryTwo.append("\n\tGROUP BY bookID");
-        //
-
-
-        // partial name search
-        StringBuilder queryThree = new StringBuilder();
-        String temp = """
-                \tSELECT bookID,
-                \t    title,
-                \t    COUNT(*) as num
-                \tFROM book
-                \tWHERE\s""";
-        for (int a = 0; a < searchTerms.length; a++) {
-            queryThree.append(temp);
-            queryThree.append(" title LIKE '%");
-            queryThree.append(searchTerms[a]);
-            queryThree.append("%'");
-            queryThree.append("\n\tGROUP BY bookID");
-            if (a != searchTerms.length-1) {
-                queryThree.append("\nUNION ALL\n\t");
-            }
-        }
-        //
-
-        // publication date search
-        String queryFour = """
-                \tSELECT bookID,
-                \t    title,
-                \t    COUNT(*) as num
-                \tFROM book
-                \tWHERE\s""" + " publicationDate >= '" +
-                (int) yearMin.getValue() + "-01" + "-01'" +
-                " AND publicationDate < '" +
-                (int) yearMax.getValue() + "-01" + "-01'" +
-                "\n\tGROUP BY bookID";
-        //
-
-        // combine the queries
-        String subquery =
-                queryOne + "\nUNION ALL\n" +
-                queryTwo + "\nUNION ALL\n" +
-                queryThree + "\nUNION ALL\n" +
-                queryFour;
-
-        StringBuilder finalQuery = new StringBuilder("""
-                SELECT SUM(num) as tNum,
-                r1.title,
-                r1.bookID
-                FROM (
-                    """);
-        finalQuery.append(subquery);
-        // post-amble
-        finalQuery.append("""
-                \n) AS r1
-                    JOIN book b2
-                        ON r1.bookID = b2.bookID
-                WHERE length >\s""");
-        finalQuery.append((int) pageCount.getValue()).append("\n");
-        finalQuery.append("""
-                GROUP BY r1.title, r1.bookID
-                HAVING tNum > 1
-                ORDER BY tnum DESC;
-                """);
         ArrayList<String[]> result;
 
         try {
             Statement st = DatabaseConnection.db().createStatement();
-            result = Query.resultSetToArrayList(st.executeQuery(finalQuery.toString()));
+            result = Query.resultSetToArrayList(st.executeQuery(search.construct()));
             if (result != null) {
                 for (String[] str : result) {
                     FXMLLoader loader = new FXMLLoader(Tools.class.getResource("/com/bkgroup/worm/controllers/SearchCard.fxml"));
@@ -182,7 +92,7 @@ public class SearchController {
                         sc.setup(img, str[1], Integer.parseInt(str[2]));
                     }
                     catch (Exception e) {
-                        System.out.println(str[1] + " " + tempStr);
+                        e.printStackTrace();
                     }
                 }
             }
---
+++ src/main/java/com/bkgroup/worm/utils/SearchBuilder.java
@@ -0,0 +1,141 @@
+package com.bkgroup.worm.utils;
+
+public class SearchBuilder {
+    boolean constructed = false;
+    int pageCount;
+    String query = "";
+
+    /**
+     * Construction method to return the finished Query String.
+     * @return the finalized query.
+     */
+    public String construct () {
+        if (!constructed) {
+            StringBuilder result = new StringBuilder("""
+                SELECT SUM(num) as tNum,
+                r1.title,
+                r1.bookID
+                FROM (
+                    """);
+            result.append(query);
+            result.append("""
+                \n) AS r1
+                    JOIN book b2
+                        ON r1.bookID = b2.bookID
+                WHERE length >\s""");
+            result.append(pageCount).append("\n");
+            result.append("""
+                GROUP BY r1.title, r1.bookID
+                HAVING tNum > 1
+                ORDER BY tnum DESC;
+                """);
+            query = result.toString();
+            constructed = true;
+        }
+        return query;
+    }
+
+    /**
+     * Search for books that contain all words of a given Array of Strings.
+     * If the query is not empty, then it will add a UNION ALL statement before it adds the rest.
+     * @param nameSections A given Array of Strings representing a title.
+     * @return this Builder object.
+     */
+    public SearchBuilder fullName (String[] nameSections) {
+        StringBuilder full = new StringBuilder("""
+                \tSELECT bookID,
+                \t    title,
+                \t    CEIL(COUNT(*)*3.5) as num
+                \tFROM book
+                \tWHERE""");
+        for (int a = 0; a < nameSections.length; a++) {
+            full.append(" title LIKE '%");
+            full.append(nameSections[a]);
+            full.append("%'");
+            if (a != nameSections.length-1) {
+                full.append(" AND");
+            }
+        }
+        full.append("\n\tGROUP BY bookID");
+        if (!query.equals("")) {
+            query+="\nUNION ALL\n";
+        }
+        query += full.toString();
+        return this;
+    }
+
+    /**
+     * Search for books that match with the given partial name.
+     * If the query is not empty, then it will add a UNION ALL statement before it adds the rest.
+     * @param pName A given partial name.
+     * @return this Builder object.
+     */
+    public SearchBuilder partialName (String pName) {
+        if (!query.equals("")) {
+            query+="\nUNION ALL\n";
+        }
+        query += """
+                \tSELECT bookID,
+                \t    title,
+                \t    COUNT(*) as num
+                \tFROM book
+                \tWHERE""" + " title LIKE '%" + pName + "%'" +
+                "\n\tGROUP BY bookID";
+        return this;
+    }
+
+    /**
+     * Search for books that match one or more genres.
+     * If the query is not empty, then it will add a UNION ALL statement before it adds the rest.
+     * @param genres an Array of Strings that represent the genres.
+     * @return this Builder object.
+     */
+    public SearchBuilder genre (String[] genres) {
+        StringBuilder wantedGenre = new StringBuilder("""
+                \tSELECT a.bookID,
+                \t    b.title,
+                \t    COUNT(*) AS num
+                \tFROM genre a
+                \t    JOIN book b
+                \t        ON a.bookID = b.bookID""");
+        wantedGenre.append("\n\tWHERE");
+        for (int a = 0; a < genres.length; a++) {
+            wantedGenre.append(" a.genre = '");
+            wantedGenre.append(genres[a]);
+            wantedGenre.append("'");
+            if (a != genres.length-1) {
+                wantedGenre.append(" OR");
+            }
+        }
+        wantedGenre.append("\n\tGROUP BY a.bookID");
+        if (!query.equals("")) {
+            query+="\nUNION ALL\n";
+        }
+        query += wantedGenre.toString();
+        return this;
+    }
+
+    /**
+     * Builder method that adds a publication field to the sub-query.
+     * If the query is not empty, then it will add a UNION ALL statement before it adds the rest.
+     * @param yearMin The minimum publication year to be specified.
+     * @param yearMax The maximum publication year to be specified.
+     * @return this Builder object.
+     */
+    public SearchBuilder publication (int yearMin, int yearMax) {
+        if (!query.equals("")) {
+            query+="\nUNION ALL\n";
+        }
+        query += """
+                \tSELECT bookID,
+                \t    title,
+                \t    COUNT(*) as num
+                \tFROM book
+                \tWHERE""" + " publicationDate >= '" +
+                yearMin + "-01" + "-01'" +
+                " AND publicationDate < '" +
+                yearMax + "-01" + "-01'" +
+                "\n\tGROUP BY bookID";
+        return this;
+    }
+}
